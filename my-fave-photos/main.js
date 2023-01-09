2f0688fa963343543d81de0d5727e68530c6d10f
MasterCoder1323 <121037342+MasterCoder1323@users.noreply.github.com>
2023-01-09 22:58:36 +0000

    commit & sync

---
+++ my-fave-photos/main.js
@@ -0,0 +1,18 @@
+//Music
+var songAlbum = [
+    "https://mastercoder1323.github.io/Byjus-Projects/assets/music/Imagine%20Dragons%20-%20Believer%20(Official%20Music%20Video).mp3"
+];
+var albumCovers = [
+    "https://i.scdn.co/image/ab67616d00001e022f2623c93d48a736c6f23cef"
+]
+var player = new album(songAlbum,photos,"player","cover");
+function playPauseM(){
+    
+}
+//Span & Div click
+function spanC(){
+    console.log("This is a Span");
+}
+function divC(){
+    console.log("This is a div");
+}
